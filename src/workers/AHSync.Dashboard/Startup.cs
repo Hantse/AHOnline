94c569c3ac2b205b44811dd01208a8352e743054
Patrick Grasseels <grasseelsp@hotmail.com>
2021-06-28 09:52:04 +0200

    Fix DI

--- src/workers/AHSync.Dashboard/Startup.cs
+++ src/workers/AHSync.Dashboard/Startup.cs
@@ -35,6 +35,7 @@ namespace AHSync.Dashboard
 
             var builder = new ContainerBuilder();
             builder.RegisterType<SqlDbConnectionFactory>().As<IDatabaseConnectionFactory>().InstancePerDependency();
+            builder.RegisterType<WoWApiService>().As<IWoWApiService>().InstancePerDependency();
             builder.RegisterType<AuctionHouseRepository>().As<IAuctionHouseRepository>().InstancePerDependency();
             builder.RegisterType<AuctionHouseService>().As<IAuctionHouseService>().InstancePerDependency();
             builder.Populate(services);
